de541db8c4b16037256e3e9699badbeb4e107ad5
skempy <scottkemp28@gmail.com>
2021-03-10 14:04:09 +0000

    added health endpoints

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,10 +1,13 @@
 package io.pivotal.pal.tracker;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
+import java.util.LinkedHashMap;
 
 @SpringBootApplication
 public class PalTrackerApplication {
